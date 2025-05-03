f1039e4ec8c4146514c749c93daf1a413b1d05e7
Seun Samuel-Ipaye <seuneipaye@gmail.com>
2025-05-03 15:55:48 -0400

    new codebase

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("/api/me", { withCredentials: true })
+      .then((res) => setUser(res.data))
+      .catch((err) => console.log("Not logged in"));
+  }, []);
+
+  const handleLogin = () => {
+    window.location.href = "/api/login";
+  };
+
+  return (
+    <div>
+      <h1>Spotify Playlist Manager</h1>
+      {!user ? (
+        <button onClick={handleLogin}>Login with Spotify</button>
+      ) : (
+        <div>
+          <p>Welcome, {user.display_name}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
